0b36336c3e453d2df4230def4db7268131c9cf83
mimigargon <mireiag77@gmail.com>
2022-05-17 14:25:19 +0200

    Iteration#2

---
+++ iteration-2.js
@@ -0,0 +1,44 @@
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let sumaVolumen = 0;
+let numSonidos = 0;
+
+for (const user of users) {
+  for (const key in user.favoritesSounds) {
+      console.log(key)
+    sumaVolumen += user.favoritesSounds[key].volume;
+    numSonidos += 1;
+  }
+}
+console.log(sumaVolumen)
+console.log(numSonidos)
+console.log("media volumenes", sumaVolumen/numSonidos);
